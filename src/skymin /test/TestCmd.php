b03ed5ec46d22c33a47b83bb3bba4fcb8e75fe1c
skymin <ppt00003333@gmail.com>
2021-12-11 11:08:23 +0900

    upload

---
+++ src/skymin /test/TestCmd.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types = 1);
+
+namespace skymin\test;
+
+use pocketmine\command\CommandSender;
+
+use skymin\CommandLib\command\{BaseCommand, EnumFactory, EnumType};
+
+class TestCmd extends BaseCommand{
+	
+	public function __construct(){
+		parent::__construct('test');
+		$this->addParameter(EnumFactory::create('pos', Enumtype::TARGET(),[], true));
+		$this->addParameter(EnumFactory::create('test', 'test', ['t', 'e', 's', 't']));
+	}
+	
+	public function execute(CommandSender $sender, string $commandLabel, array $args) :void{
+		return;
+	}
+	
+}
